ae110ed8928051e01e05ac535f703ffbd572b2af
Alfredocaconda <123181679+Alfredocaconda@users.noreply.github.com>
2024-02-22 09:18:58 -0300

    tamplate-dashboard-feito

---
+++ resources/views/autores.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.corpo')
+@extends('layout.cabecalho')
+@section('title','Autores')
+
+@section('cabecalho')
+@section('content')
+<section class="mb-30px">
+      <div class="container">
+        <div class="inputs" >
+        <input type="text" style="width: 50%;" placeholder="Buscar os autores dos artigos Cientificos">
+      <button  class="botao">Pesquisar</button>
+      <ul class="ul">
+        <li class="li">Coleções</li>
+        <li class="li">Periodicos</li>
+      </ul>
+        </div>
+      </div>
+    </section>
+  <!--================ Start Footer Area =================-->
+  @endsection
+
+
+
+
+
